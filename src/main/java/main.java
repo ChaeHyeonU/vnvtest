1c6b240e7fb725d1b4133c45cd0715b03e68dc2a
ChaeHyeonU <wwwa0528@gmail.com>
2021-04-28 16:50:37 +0900

    webhook test success

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,8 +4,8 @@ public class main {
     public static void main(String[] args) {
 
         System.out.println("123123123");
-        System.out.println("123123123");
 
+        System.out.println("123123123");
     }
 }
 
